df05a95f5ebd6e7184bb5cc74462acaeabdcb1b0
Kerwin Marquez <kerwin.marquez24@gmail.com>
2020-06-16 23:02:10 -0500

    Actualizacion de Relay por Led

--- server_led.js
+++ server_led.js
@@ -4,37 +4,43 @@ var io = require('socket.io')(http);
 
 var five = require("johnny-five");
 
-
-
-
-
 var PORT = process.env.PORT || 8080;
-
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/led_control.html');
 
 });
+
+
+var relay;
 var led;
 var board = new five.Board();
 board.on("ready", function() {
-    led = new five.Led(13);
+     relay = new five.Relay(13);
+     led = new five.Led(13);
 });
 
 io.on('connection', function(socket) {
     //this.pinMode(13, five.Pin.OUTPUT);
     socket.on('turn_on', function(msj) {
         //this.digitalWrite(13, 1);
-        led.on();
+        relay.on();
         socket.emit('status', 'Led Encendido');
         console.log('on');
+        
+       setTimeout(() => {
+           relay.off();
+       }, 3000);
+        
     });
+    
     socket.on('turn_off', function(msj) {
         //this.digitalWrite(13, 0);
         led.off();
         socket.emit('status', 'Led Apagado');
         console.log('off');
     });
+    
 });
 
 
